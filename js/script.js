8a1d26a4d1ac4b8e1cdf84c151337b14bce514b7
dgssdagdg <100527555+dgssdagdg@users.noreply.github.com>
2023-01-18 20:42:11 +0300

    add

--- js/script.js
+++ js/script.js
@@ -21,4 +21,37 @@ window.addEventListener('click', function(event) {
     } else if (!event.target.closest('.pop-up-content') && ev.closest('.pop-up-input-open')) {
         ev.classList.remove('pop-up-input-open');
     }
+
+    if (event.target.closest('.table-mini-top-text')) {
+        let tableBlock = event.target.closest('.table-mini-block');
+        tableBlock.classList.toggle('table-mini-block-active')
+    }
+
+    if (event.target.closest('.tc-select')) {
+        let tcSelect = event.target.closest('.tc-select');
+        tcSelect.classList.toggle('tc-select-open');
+    }
+    let tcInput = document.querySelector('.tc-select-top-input')
+    if (event.target.closest('.tc-select-top-item')) {
+        let tcSelect = event.target.closest('.tc-select');
+        let tcInput = tcSelect.querySelector('.tc-select-top-input')
+        let popClick = event.target.textContent;
+        tcInput.value = popClick;
+    }
+    let tcThanks = document.querySelector('.pop-up-thanks');
+    if (event.target.closest('.pop-up-thanks-open')) {
+        tcThanks.classList.toggle('pop-up-thanks-active');
+    } else if (!event.target.closest('.pop-up-content') && tcThanks.closest('.pop-up-thanks-active')) {
+        tcThanks.classList.remove('pop-up-thanks-active');
+    } else if (event.target.closest('.pop-up-thanks-btn')) {
+        tcThanks.classList.remove('pop-up-thanks-active');
+        popUp.classList.remove('pop-up-open');
+    }
+})
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
 })
